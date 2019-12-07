2b68876b75d348071023708b55f10ddc1bdc56e7
PawankumarPK <pk9910765616@gmail.com>
2019-12-07 13:19:23 +0530

    switch adatpter to fragment

--- app/src/main/java/com/project/newconsoleapp/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/project/newconsoleapp/adapter/RecyclerViewAdapter.kt
@@ -1,12 +1,18 @@
 package com.project.newconsoleapp.adapter
 
+import android.content.Context
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.project.newconsoleapp.R
+import com.project.newconsoleapp.activity.BaseActivity
 import com.project.newconsoleapp.api.models.DataList
+import com.project.newconsoleapp.fragment.Core0Fragment
 import kotlinx.android.synthetic.main.view_holder.view.*
+
 
 /**
  * Created by pawan on 05,December,2019
@@ -15,8 +21,15 @@ class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>
 
     private var itemList: ArrayList<DataList> = ArrayList()
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.view_holder,parent, false))
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.view_holder,
+                parent,
+                false
+            )
+        )
     }
 
     override fun getItemCount(): Int {
@@ -25,6 +38,7 @@ class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bindItems(position)
+
     }
 
 
@@ -33,11 +47,38 @@ class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>
         fun bindItems(position: Int) {
             val data = itemList[position]
             itemView.mRecyclerTextView.text = data.mac
+            itemView.setOnClickListener {
+                Toast.makeText(itemView.context, "Item is clicked ${data.mac}", Toast.LENGTH_SHORT).show()
+                
+                val activity = itemView.context as BaseActivity
+                val args = Bundle()
+                args.putString("mac_id", data.mac)
+                val myFragment = Core0Fragment()
+                myFragment.arguments = args
+                activity.supportFragmentManager.beginTransaction()
+                    .replace(R.id.mFrameContainer, myFragment).addToBackStack(null).commit()
+
+
+            }
+
         }
 
     }
 
-     fun getDataIntoItemlist(list: ArrayList<DataList>){
+    private fun demoFun() {
+        // val singleAmount = Integer.parseInt(carSingleAmount.text.toString())
+
+
+        /*  val args = Bundle()
+          args.putString("doctor_id", carType.text.toString())
+          val newFragment = CarDetails()
+          newFragment.arguments = args
+
+          fragmentManager!!.beginTransaction().replace(R.id.containerView, newFragment).commit()*/
+    }
+
+
+    fun getDataIntoItemlist(list: ArrayList<DataList>) {
         itemList = list
 
     }
