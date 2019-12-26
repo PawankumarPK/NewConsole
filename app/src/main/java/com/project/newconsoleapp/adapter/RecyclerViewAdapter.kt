f7feaa391042c642d6b24397cae1fbaa2804cb96
PawankumarPK <pk9910765616@gmail.com>
2019-12-26 22:45:01 +0530

    Add status fragment & update all

--- app/src/main/java/com/project/newconsoleapp/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/project/newconsoleapp/adapter/RecyclerViewAdapter.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.project.newconsoleapp.R
 import com.project.newconsoleapp.activity.BaseActivity
 import com.project.newconsoleapp.api.models.DataList
+import com.project.newconsoleapp.fragment.StatusFragment
 import kotlinx.android.synthetic.main.view_holder.view.*
 
 
@@ -19,15 +20,8 @@ class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>
 
     private var itemList: ArrayList<DataList> = ArrayList()
 
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.view_holder,
-                parent,
-                false
-            )
-        )
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.view_holder, parent, false))
     }
 
     override fun getItemCount(): Int {
@@ -39,42 +33,36 @@ class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>
 
     }
 
-
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         fun bindItems(position: Int) {
             val data = itemList[position]
-            itemView.mRecyclerTextView.text = data.mac
-            itemView.setOnClickListener {
-                Toast.makeText(itemView.context, "Item is clicked ${data.mac}", Toast.LENGTH_SHORT).show()
+            itemView.mBotName.text = data.mac
+            itemView.setOnClickListener { sendDataIntoFragment(position) }
+        }
 
-                val activity = itemView.context as BaseActivity
-                val args = Bundle()
-                args.putString("mac_id", data.mac)
-                val myFragment = Core0Fragment()
-                myFragment.arguments = args
-                activity.supportFragmentManager.beginTransaction()
-                    .replace(R.id.mFrameContainer, myFragment).addToBackStack(null).commit()
+        private fun sendDataIntoFragment(position: Int){
+            val data = itemList[position]
 
+            val activity = itemView.context as BaseActivity
+            val args = Bundle()
+            args.putString("mac_id", data.mac!!.toString())
+            args.putString("joinstamp", data.joinstamp!!.toString())
+            args.putString("updatestamp", data.updatestamp!!.toString())
+            args.putString("stats", data.stats.screens.toString().replace("[", "").replace("]", ""))
+            args.putString("P",data.stats.battery.P.toString())
+            args.putString("SOC",data.stats.battery.SOC.toString())
+            args.putString("V",data.stats.battery.V.toString())
+            args.putString("CE",data.stats.battery.CE.toString())
+            args.putString("I",data.stats.battery.I.toString())
 
-            }
-
+            val myFragment = StatusFragment()
+            myFragment.arguments = args
+            activity.supportFragmentManager.beginTransaction()
+                .replace(R.id.mFrameContainer, myFragment).addToBackStack(null).commit()
         }
 
     }
-
-    private fun demoFun() {
-        // val singleAmount = Integer.parseInt(carSingleAmount.text.toString())
-
-
-        /*  val args = Bundle()
-          args.putString("doctor_id", carType.text.toString())
-          val newFragment = CarDetails()
-          newFragment.arguments = args
-
-          fragmentManager!!.beginTransaction().replace(R.id.containerView, newFragment).commit()*/
-    }
-
 
     fun getDataIntoItemlist(list: ArrayList<DataList>) {
         itemList = list
